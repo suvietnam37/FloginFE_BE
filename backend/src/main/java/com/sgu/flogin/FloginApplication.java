dd08df534d5140005952e01115b63b6f38fe264f
SuVietNam <suvietnam37@gmail.com>
2025-10-28 11:03:09 +0700

    feat: Initial setup for Frontend (React) and Backend (Spring Boot)

---
+++ backend/src/main/java/com/sgu/flogin/FloginApplication.java
@@ -0,0 +1,13 @@
+package com.sgu.flogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloginApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sgu/flogin/FloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgu.flogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
