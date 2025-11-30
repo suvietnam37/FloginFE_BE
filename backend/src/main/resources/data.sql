bc42eab7b592f273cc1676fc6f354bdc91438439
SuVietNam <suvietnam37@gmail.com>
2025-11-30 11:48:24 +0700

    complete CI/CD lan 6

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(username, password) VALUES ('testuser', '$2a$10$xR.wC3EG4BVtCc5Q3SqGXupjK/fX9jw2Uec.Qr8LwyYhA4yWa.qgu');
+
+INSERT INTO products (name, price, quantity) VALUES ('Laptop Dell XPS 15', 35000000, 10);
+INSERT INTO products (name, price, quantity) VALUES ('Bàn phím cơ Keychron K2', 2500000, 50);
